cc5b0979fa13c5562f115da0b410ce95ba7d0cc6
ara_d <d.ara@clinsoftcsd.com>
2022-02-10 18:19:33 +0400

    test commit

--- App.js
+++ App.js
@@ -1,11 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { LocaleConfig } from 'react-native-calendars';
+import { Alert, Modal, StyleSheet, Text, Pressable, View, TextInput } from "react-native";
+import { useState } from "react"
 
+LocaleConfig.locales['fr'] = {
+  monthNames: [
+    '	January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ],
+  monthNamesShort: ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.'],
+  dayNames: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  dayNamesShort: ['Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.', 'Sun.'],
+  today: "Today"
+};
+LocaleConfig.defaultLocale = 'fr';
 export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false)
+  const [currentDate, setCurrentDate] = useState("")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" /> */}
+      <Calendar
+        onDayPress={day => {
+          setModalIsVisible(true)
+          setCurrentDate(day.dateString)
+          console.log('selected day', day.dateString);
+        }}
+      />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalIsVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalIsVisible(!modalIsVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            {/* <TextInput
+              style={styles.input}
+            // onChangeText={onChangeText}
+            // value={text}
+            /> */}
+            <TextInput
+              style={styles.input}
+              // onChangeText={onChangeNumber}
+              // value={number}
+              placeholder="useless placeholder"
+              keyboardType="numeric"
+            />
+            <Text style={styles.modalText}>{currentDate}</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalIsVisible(!modalIsVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -16,5 +84,47 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    // paddingTop: "2rem"
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 });
